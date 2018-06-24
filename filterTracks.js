961c67758dfa78a3901168ca6fccc7fc011bc320
Cam Jackson <camjackson89@gmail.com>
2018-06-24 15:42:13 +0800

    Filter out live tracks

--- filterTracks.js
+++ filterTracks.js
@@ -5,13 +5,15 @@ const maxAcousticness = 0.9;
 const minTempo = 160;
 const maxTempo = 200;
 const minDuration = 30000;
+const maxLiveness = 0.8;
 
 console.log(`Filtering ${tracks.length} tracks`);
 const filtered = tracks.filter(track => (
   track.acousticness <= maxAcousticness &&
   track.tempo >= minTempo &&
   track.tempo <= maxTempo &&
-  track.duration_ms >= minDuration
+  track.duration_ms >= minDuration &&
+  track.liveness <= maxLiveness
 ));
 
 console.log(`Reduced down to ${filtered.length} tracks`);
